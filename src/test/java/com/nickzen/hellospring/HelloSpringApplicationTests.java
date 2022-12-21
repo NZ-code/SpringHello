30e8fded06589c5ba78fe2d5bd304f2b9f3ddf93
Nick <zenevichnikita2001@gmail.com>
2022-12-21 16:32:18 +0100

    Initial commit

---
+++ src/test/java/com/nickzen/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickzen.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
